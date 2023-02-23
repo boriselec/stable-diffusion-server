5b57f7cf5f890b24151d242e7878387b3887db21
boriselec <boris.puzikov@gmail.com>
2023-02-23 05:55:17 +0300

    manually clean memory to avoid torch allocation error

--- generator.py
+++ generator.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
 from types import SimpleNamespace
+import gc
 import stablediffusion
 
 
 def generate_loop(job_queue, curr_rq, startup_args):
-    args = SimpleNamespace(device=startup_args.device,
-                           model=startup_args.model,
-                           half=startup_args.half,
-                           height=startup_args.height,
-                           width=startup_args.width,
-                           steps=startup_args.steps,
-                           token='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',
-                           skip=True,
-                           seed=519762248,
-                           scale=7.5,
-                           strength=0.75,
-                           image=None,
-                           image_scale=None,
-                           negative_prompt=None,
-                           scheduler=None,
-                           xformers_memory_efficient_attention=False,
-                           attention_slicing=False,
-                           mask=None)
     while True:
+        args = SimpleNamespace(device=startup_args.device,
+                               model=startup_args.model,
+                               half=startup_args.half,
+                               height=startup_args.height,
+                               width=startup_args.width,
+                               steps=startup_args.steps,
+                               token='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',
+                               skip=True,
+                               seed=519762248,
+                               scale=7.5,
+                               strength=0.75,
+                               image=None,
+                               image_scale=None,
+                               negative_prompt=None,
+                               scheduler=None,
+                               xformers_memory_efficient_attention=False,
+                               attention_slicing=False,
+                               mask=None)
         try:
             job = job_queue.get()
             curr_rq.value = job.rq
@@ -32,4 +33,7 @@ def generate_loop(job_queue, curr_rq, startup_args):
         except Exception as e:
             print(e, flush=True)
             pass
+        del pipeline
+        del args
+        gc.collect()
         curr_rq.value = None
