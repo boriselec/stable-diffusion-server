07207a5a51f74a46ba5a16bd34857913a6e38761
boriselec <boris.puzikov@gmail.com>
2023-03-14 08:32:18 +0300

    lock: one request at time

--- server.py
+++ server.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-import argparse
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
 import copy
 import importlib
 import os
+import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 generator = importlib.import_module("docker-entrypoint")
 
@@ -13,26 +12,34 @@ serverPort = 8081
 
 
 class GenerationServer(BaseHTTPRequestHandler):
+    lock = threading.Lock()
+
     def do_POST(self):
         if self.path.startswith('/generate'):
-            self.send_response(200)
+            free = self.lock.acquire(blocking=False)
+            if not free:
+                self.send_error(503, 'Busy')
+            try:
+                self.send_response(200)
 
-            content_length = int(self.headers['Content-Length'])
-            data = self.rfile.read(content_length).decode('utf-8')
-            args = copy.deepcopy(startup_args)
-            args.prompt = data
+                content_length = int(self.headers['Content-Length'])
+                data = self.rfile.read(content_length).decode('utf-8')
+                args = copy.deepcopy(startup_args)
+                args.prompt = data
 
-            pipeline = generator.stable_diffusion_pipeline(args)
-            img_paths = generator.stable_diffusion_inference(pipeline)
+                pipeline = generator.stable_diffusion_pipeline(args)
+                img_paths = generator.stable_diffusion_inference(pipeline)
 
-            f = open(img_paths[0], 'rb')
-            self.send_header("Content-type", "image/png")
-            self.end_headers()
-            self.wfile.write(f.read())
-            f.close()
+                f = open(img_paths[0], 'rb')
+                self.send_header("Content-type", "image/png")
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
 
-            for img_path in img_paths:
-                os.remove(img_path)
+                for img_path in img_paths:
+                    os.remove(img_path)
+            finally:
+                self.lock.release()
 
         else:
             self.send_error(404, 'Not found')
